fe96b24b575afa270ceef0707488782bb1e3c278
Pascal Sommer <p@pascalsommer.ch>
2020-05-04 06:04:02 +0200

    get rid of std::reference_wrapper

--- include/treeer.hpp
+++ include/treeer.hpp
@@ -1,7 +1,6 @@
 #ifndef TREEER_HPP
 #define TREEER_HPP
 
-#include <functional>
 #include <vector>
 #include <memory>
 #include <optional>
@@ -9,15 +8,11 @@
 constexpr unsigned int N = 3;
 
 struct tree_node{
-    unsigned int color;
-    std::vector<std::unique_ptr<tree_node>> children;
-    // std::vector<std::reference_wrapper<std::unique_ptr<tree_node>>> flat;
+    std::vector<std::unique_ptr<tree_node>> children {};
+    unsigned int color {0};
 };
 
-template <typename T>
-using opt_ref = std::optional<std::reference_wrapper<T>>;
-
-opt_ref<const tree_node> get_valid_starting_point(const tree_node& t, int nr_required_children);
+const tree_node* get_valid_starting_point(const tree_node& t, int nr_required_children);
 
 bool is_embeddable(const tree_node& smol,const tree_node& big);
 
--- src/treeer.cpp
+++ src/treeer.cpp
@@ -2,16 +2,16 @@
 #include "treeer.hpp"
 #include "combinatorics.hpp"
 
-opt_ref<const tree_node> get_valid_starting_point(const tree_node& t, int nr_required_children){
+const tree_node* get_valid_starting_point(const tree_node& t, int nr_required_children){
     if (t.children.size() >= nr_required_children)
-        return t;
+        return &t;
 
     for (auto const& c : t.children) {
         if (c->children.size() >= nr_required_children)
-            return *c;
+            return c.get();
     }
 
-    return std::nullopt;
+    return nullptr;
 }
 
 bool is_embeddable(const tree_node& smol,const tree_node& big){
@@ -25,11 +25,13 @@ bool is_embeddable(const tree_node& smol,const tree_node& big){
     if (!root)
         return false;
 
+    nr_big_children = root->children.size();
+
     std::vector<std::vector<bool>> embeddable(nr_big_children, std::vector<bool>(nr_smol_children));
 
     for (int i = 0; i < nr_big_children; ++i)
         for (int j = 0; j < nr_smol_children; ++j)
-            embeddable[i][j] = is_embeddable(*smol.children[j], *big.children[i]);
+            embeddable[i][j] = is_embeddable(*smol.children[j], *root->children[i]);
 
     std::vector<int> k_permutation(nr_smol_children);
     std::iota(k_permutation.begin(), k_permutation.end(), 0);
