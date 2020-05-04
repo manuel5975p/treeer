43ffdf9b270632d90944e9c7fb609733bff1ed79
Pascal Sommer <p@pascalsommer.ch>
2020-05-04 06:04:10 +0200

    add tests for get_valid_starting_point

--- test/treeer.cpp
+++ test/treeer.cpp
@@ -2,7 +2,25 @@
 
 #include "treeer.hpp"
 
-TEST_CASE("find possible root")
+TEST_CASE("get_valid_starting_point")
 {
-	CHECK(true);
+	tree_node singleton {};
+
+	auto start = get_valid_starting_point(singleton, 0);
+	REQUIRE(start);
+	CHECK(start == &singleton);
+	CHECK(get_valid_starting_point(singleton, 1) == nullptr);
+
+	tree_node one_child {};
+	one_child.children.push_back(std::make_unique<tree_node>());
+	start = get_valid_starting_point(one_child, 0);
+	REQUIRE(start);
+	CHECK(start == &one_child);
+
+	start = get_valid_starting_point(one_child, 1);
+	REQUIRE(start);
+	CHECK(start == &one_child);
+
+	start = get_valid_starting_point(one_child, 2);
+	CHECK(!start);
 }
